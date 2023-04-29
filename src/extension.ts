d6d389d37636aee5f5dccb97ab26352fb7c5902f
guangyusong <15316444+guangyusong@users.noreply.github.com>
2023-04-29 16:13:15 -0400

    Parse keys and actions from table

--- src/extension.ts
+++ src/extension.ts
@@ -11,41 +11,69 @@ export function activate(context: vscode.ExtensionContext) {
 function generateDescriptionFromP4Code(p4Code: string): string {
 	const controlRegex = /control\s+(\w+)\s*\(/;
 	const tableRegex = /table\s+(\w+)\s*\{/;
-	const actionRegex = /action\s+(\w+)\s*\(/;
+	const keyLineRegex = /\s*key\s*=\s*\{/;
+	const actionsLineRegex = /\s*actions\s*=\s*\{/;
+	const sizeRegex = /\s*size\s*=\s*(\d+)\s*;/;
+	const defaultActionRegex = /\s*default_action\s*=\s*(\w+)\s*;/;
 
-	let controls: { [key: string]: { tables: string[]; actions: string[] } } = {};
+	let controls: { [key: string]: { tables: { [key: string]: { key: string[]; actions: string[]; size: string; default_action: string } } } } = {};
 
 	let lines = p4Code.split('\n');
 	let currentControl = '';
+	let currentTable = '';
+	let parsingKeys = false;
+	let parsingActions = false;
 
 	for (const line of lines) {
 		let controlMatch = controlRegex.exec(line);
 		let tableMatch = tableRegex.exec(line);
-		let actionMatch = actionRegex.exec(line);
+		let keyLineMatch = keyLineRegex.exec(line);
+		let actionsLineMatch = actionsLineRegex.exec(line);
+		let sizeMatch = sizeRegex.exec(line);
+		let defaultActionMatch = defaultActionRegex.exec(line);
 
 		if (controlMatch) {
 			currentControl = controlMatch[1];
-			controls[currentControl] = { tables: [], actions: [] };
+			controls[currentControl] = { tables: {} };
 		} else if (tableMatch && currentControl) {
-			controls[currentControl].tables.push(tableMatch[1]);
-		} else if (actionMatch && currentControl) {
-			controls[currentControl].actions.push(actionMatch[1]);
+			currentTable = tableMatch[1];
+			controls[currentControl].tables[currentTable] = { key: [], actions: [], size: '', default_action: '' };
+		} else if (currentControl && currentTable) {
+			if (keyLineMatch) {
+				parsingKeys = true;
+			} else if (actionsLineMatch) {
+				parsingActions = true;
+			} else if (parsingKeys) {
+				if (line.trim() === '}') {
+					parsingKeys = false;
+				} else {
+					controls[currentControl].tables[currentTable].key.push(line.trim());
+				}
+			} else if (parsingActions) {
+				if (line.trim() === '}') {
+					parsingActions = false;
+				} else {
+					controls[currentControl].tables[currentTable].actions.push(line.trim());
+				}
+			} else if (sizeMatch) {
+				controls[currentControl].tables[currentTable].size = sizeMatch[1];
+			} else if (defaultActionMatch) {
+				controls[currentControl].tables[currentTable].default_action = defaultActionMatch[1];
+			}
 		}
 	}
 
 	let description = "The system consists of the following components:\n";
 	for (const control in controls) {
 		description += `• Control: ${control}\n`;
-		if (controls[control].tables.length > 0) {
+		if (Object.keys(controls[control].tables).length > 0) {
 			description += "  ◦ Tables:\n";
-			for (const table of controls[control].tables) {
+			for (const table in controls[control].tables) {
 				description += `    ▪ ${table}\n`;
-			}
-		}
-		if (controls[control].actions.length > 0) {
-			description += "  ◦ Actions:\n";
-			for (const action of controls[control].actions) {
-				description += `    ▪ ${action}\n`;
+				description += `      ▫ Key: ${controls[control].tables[table].key.join(', ')}\n`;
+				description += `      ▫ Actions: ${controls[control].tables[table].actions.join(', ')}\n`;
+				description += `      ▫ Size: ${controls[control].tables[table].size}\n`;
+				description += `      ▫ Default Action: ${controls[control].tables[table].default_action}\n`;
 			}
 		}
 	}
